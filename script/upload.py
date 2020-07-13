07846da118002416ce3bf7c7dcb19d5f445a1a89
Rainer Simon <rainer.simon@ait.ac.at>
2020-07-13 10:35:01 +0200

    Multi-part multi-doc upload

--- script/upload.py
+++ script/upload.py
@@ -1,8 +1,34 @@
 import config as cfg
+import glob
 import requests
+
+from os import listdir
+from os.path import isdir, join
 
 # Global client session
 session = requests.Session()
+
+#####
+# Checks the data folder and compiles the 'documents' we need
+# to upload to Recogito
+#####
+def get_documents_to_upload():
+  directories = [ d for d in listdir(cfg.DOCUMENT_FOLDER) if isdir(join(cfg.DOCUMENT_FOLDER, d)) ]
+  directories.sort()
+
+  documents = []
+
+  for directory in directories:
+    doc = { 'title': directory, 'files': [] }
+
+    documents.append(doc)
+
+    for filename in glob.glob(join(cfg.DOCUMENT_FOLDER, directory) + '**/*.*'):
+      doc['files'].append(filename)
+
+    doc['files'].sort()
+
+  return documents
 
 #####
 # Login to Recogito
@@ -33,31 +59,46 @@ def upload_file(filepath, upload_id):
 def finalize_document(upload_id, session):
   return session.post(f'{cfg.RECOGITO_URL}/my/upload/{upload_id}/finalize')
 
+#####
+# 'State logic' for uploading one document (with multiple files)
+#####
+def upload_one(document):
+  print(f'Initiating upload: {document["title"]}')
 
-###############################
-#
-# Upload process starts here
-#
-###############################
-try:
-  response = login()
+  upload_id = init_new_document(document['title'])
 
-  if response.status_code != 200:
-    raise Exception(f'Login failed with code {response.status_code}')
+  for f in document['files']:
+    response = upload_file(f, upload_id)
 
-  print('Login successful - Initiating upload')
-  upload_id = init_new_document('My first scripted upload')
-  response = upload_file('../data/sample.txt', upload_id)
-
-  if response.status_code != 200:
-    raise Exception(f'Upload failed with code: {response.status_code}')
+    if response.status_code != 200:
+      raise Exception(f'Upload failed with code: {response.status_code}')
 
   response = finalize_document(upload_id, session)
 
   if response.status_code != 200:
     raise Exception(f'Could not finalize upload - failed with code: {response.status_code}')
 
-  print('Upload complete')
+  print('Upload successful')
+
+###############################
+#
+# Upload process starts here
+#
+###############################
+documents = get_documents_to_upload()
+
+print(f'Uploading {len(documents)} documents')
+
+try:
+  response = login()
+
+  if response.status_code != 200:
+    raise Exception(f'Login failed with code {response.status_code}')
+
+  print('Login successful')
+
+  for d in documents:
+    upload_one(d)
 
 except:
   print(f'Login error: {response.status_code}')
